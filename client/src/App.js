574fa4a458e483087c3ace1957080ec2cd3a51a5
SparshJain2000 <jainsparsh0801@gmail.com>
2020-11-25 16:46:09 +0530

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/index.page";
+import Event from "./pages/event.page";
+import About from "./pages/about.page";
+import Navbar from "./components/navbar.component";
 function App() {
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <Navbar />
+            <main>
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/about' component={About} />
+                    <Route path='/event' component={Event} />
+                </Switch>
+            </main>
+        </BrowserRouter>
     );
 }
 
