7e0497cf3632462540e1702f6998a0b540970430
SparshJain2000 <jainsparsh0801@gmail.com>
2020-11-28 00:21:44 +0530

    add sponsor page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/index.page";
 import Event from "./pages/event.page";
 import About from "./pages/about.page";
+import Sponsor from "./pages/sponsor.page";
 import Navbar from "./components/navbar.component";
 import Footer from "./components/footer.component";
 function App() {
@@ -13,6 +14,7 @@ function App() {
                     <Route path='/' exact component={Home} />
                     <Route path='/about' component={About} />
                     <Route path='/event' component={Event} />
+                    <Route path='/sponsors' component={Sponsor} />
                 </Switch>
             </main>
             <Footer />
---
+++ client/src/pages/sponsor.page.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Event = () => {
+    return <h1>Sponsor page ✌</h1>;
+};
+export default Event;
