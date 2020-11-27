5fc479f4c32aca24515ee521c52b52dae27e31ae
SparshJain2000 <jainsparsh0801@gmail.com>
2020-11-27 23:41:22 +0530

    add bottom padding for diff breakpoints

--- client/src/pages/index.page.js
+++ client/src/pages/index.page.js
@@ -1,5 +1,18 @@
 import React from "react";
 const Index = () => {
-    return <h1>Index page ✌</h1>;
+    return (
+        <div className='jumbotron'>
+            <h1>Index page ✌</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+                ipsum velit a odio. Nemo culpa quisquam dolore neque reiciendis
+                doloremque qui natus et eligendi eius vel accusamus, quia
+                dignissimos illo? Lorem ipsum dolor sit, amet consectetur
+                adipisicing elit. Cumque optio officia, id alias ipsum ab omnis
+                sint autem fuga praesentium incidunt molestiae soluta, amet rem.
+                Aperiam modi sint omnis a.
+            </p>
+        </div>
+    );
 };
 export default Index;
