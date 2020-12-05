35f4caefc48e7dd6ab32b7d38f6c25761e2178d6
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-05 17:36:42 +0530

    Update event page

--- client/src/pages/event.page.js
+++ client/src/pages/event.page.js
@@ -1,5 +1,17 @@
 import React from "react";
 const Event = () => {
-    return <h1>Event page ✌</h1>;
+    return (
+        <div className='col-12 col-md-11 col-lg-10 mx-auto my-2'>
+            <h1>Events</h1>
+            <hr className='line' />
+            <div>
+                The ASCE 2021 Southeast Student Conference will consist of
+                National competetions, Inhouse events and Workshops.
+            </div>
+            <div>National goes here</div>
+            <div>inhouse...</div>
+            <div>workshops...</div>
+        </div>
+    );
 };
 export default Event;
