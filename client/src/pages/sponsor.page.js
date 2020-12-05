3c98ac79974c66d12173bc167c57dff546f54a4d
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-05 17:37:13 +0530

    add sponsors page

--- client/src/pages/sponsor.page.js
+++ client/src/pages/sponsor.page.js
@@ -1,5 +1,26 @@
 import React from "react";
+import "../styles/sponsor.css";
 const Event = () => {
-    return <h1>Sponsor page ✌</h1>;
+    return (
+        <div className='col-12 col-md-11 col-lg-10 mx-auto my-2'>
+            <h1>Our Sponsors</h1>
+            <hr className='line' />
+            <br />
+            <div>
+                <div className='box'>
+                    <h5>Gold Sponsors</h5>
+                </div>
+                <br />
+                <div className='box'>
+                    <h5>Silver Sponsors</h5>
+                </div>
+                <br />
+                <div className='box'>
+                    <h5>Copper Sponsors</h5>
+                </div>
+                <br />
+            </div>
+        </div>
+    );
 };
 export default Event;
