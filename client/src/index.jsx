96a5ed48d53b11fa55e600437616e0f6a4c00c3f
SparshJain2000 <jainsparsh0801@gmail.com>
2021-01-27 22:37:26 +0530

    import debug

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.css";
-import App from "./App";
+import App from "./App.jsx";
 
 ReactDOM.render(
     <React.StrictMode>
