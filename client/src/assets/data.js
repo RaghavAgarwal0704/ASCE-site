dd06315c5c4e7843ce49178e60994a70d8869684
SparshJain2000 <jainsparsh0801@gmail.com>
2021-02-02 17:02:53 +0530

    add contact data

---
+++ client/src/assets/data.js
@@ -0,0 +1,32 @@
+import Brijesh from "../assets/Brijesh.jpg";
+const members = [
+    {
+        name: "Dr. A. N. Brijesh Nair",
+        email: "brijeshnair.an@vit.ac.in",
+        img: Brijesh,
+        phone: "+91-9944642418",
+        role: "Faculty coordinator",
+    },
+    {
+        name: "Dr. A. N. Brijesh Nair",
+        email: "brijeshnair.an@vit.ac.in",
+        img: Brijesh,
+        phone: "+91-9944642418",
+        role: "Faculty coordinator",
+    },
+    {
+        name: "Dr. A. N. Brijesh Nair",
+        email: "brijeshnair.an@vit.ac.in",
+        img: Brijesh,
+        phone: "+91-9944642418",
+        role: "Faculty coordinator",
+    },
+    {
+        name: "Dr. A. N. Brijesh Nair",
+        email: "brijeshnair.an@vit.ac.in",
+        img: Brijesh,
+        phone: "+91-9944642418",
+        role: "Faculty coordinator",
+    },
+];
+export { members };
