c185158b476a383a1d540ce42bc1c05f79f7f777
Raghav Agarwal <raghav.agarwal2018@vitstudent.ac.in>
2021-02-02 18:59:55 +0530

    Update data.js

--- client/src/assets/data.js
+++ client/src/assets/data.js
@@ -1,4 +1,5 @@
-import Brijesh from "../assets/Brijesh.jpg";
+import Brijesh from "../assets/Brijesh.jpg";-
+import utkarsh from "../assets/utkarsh.jpg";
 const members = [
     {
         name: "Dr. A. N. Brijesh Nair",
@@ -8,11 +9,11 @@ const members = [
         role: "Faculty coordinator",
     },
     {
-        name: "Dr. A. N. Brijesh Nair",
-        email: "brijeshnair.an@vit.ac.in",
-        img: Brijesh,
-        phone: "+91-9944642418",
-        role: "Faculty coordinator",
+        name: "Utkarsh Jaiswal",
+        email: "utkarsh.jaiswal2018@vitstudent.ac.in",
+        img: utkarsh,
+        phone: "+91-8840274121
+        role: "Director Finance
     },
     {
         name: "Dr. A. N. Brijesh Nair",
