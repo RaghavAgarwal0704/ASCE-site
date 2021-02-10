2a0f62e3aaf81192a9dbf874f3fa091044157122
SparshJain2000 <jainsparsh0801@gmail.com>
2021-02-11 00:02:54 +0530

    update judges card

--- client/src/pages/judge.page.jsx
+++ client/src/pages/judge.page.jsx
@@ -8,30 +8,31 @@ const Judge = () => {
                 <hr className='line' />
             </div>
             {judges.map((user) => (
-                <div className='col-12 col-md-3  px-0'>
-                    <div className='contact-card '>
-                        <div className='contact-img mx-auto'>
+                <div className='col-12 px-1 py-2'>
+                    <div className='judge-card row'>
+                        <div className='judge-img col-12 col-md-4'>
                             <img
                                 className='img-fluid my-auto mx-auto'
                                 src={user.img}
                                 alt=''
                             />
                         </div>
-                        <div className='content text-align-center'>
-                            <h5>{user.name} </h5>
-                            <h6>
-                                <strong>{user.role}</strong>
-                            </h6>
-                            <p>
-                                {user.phone}
-                                <br />
-                                <a
-                                    href={`mailto:${user.email}`}
-                                    target='_blank'
-                                    rel='noreferrer'>
-                                    {user.email}
-                                </a>
-                            </p>
+                        <div className='judge-content  col-12 col-md-8'>
+                            <h4>
+                                <strong>{user.name}</strong>
+                            </h4>
+                            <h5>
+                                {user.role.slice(
+                                    0,
+                                    user.role.indexOf("for") + 4,
+                                )}
+                                <strong>
+                                    {user.role.slice(
+                                        user.role.indexOf("for") + 4,
+                                    )}
+                                </strong>
+                            </h5>
+                            <p>{user.about}</p>
                         </div>
                     </div>
                 </div>
