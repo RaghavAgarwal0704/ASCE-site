3be6055f65142129b0d4d6a169baa15e58d787ab
SparshJain2000 <jainsparsh0801@gmail.com>
2021-02-16 21:31:33 +0530

    change hashrouter to browserrouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/index.page";
 import EventPage from "./pages/event.page";
 import About from "./pages/about.page";
@@ -15,7 +15,7 @@ import Footer from "./components/footer.component";
 import Event from "./components/event.component";
 function App() {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <Navbar />
             <main>
                 <Switch>
@@ -45,7 +45,7 @@ function App() {
                 </Switch>
             </main>
             <Footer />
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
